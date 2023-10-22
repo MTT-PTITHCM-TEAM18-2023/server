a2e8c93004668a4b477e85a7465d27dfee4628f8
Nhan Le <nhan@treehouse.finance>
2023-10-22 21:09:38 +0700

    update db authen

--- src/database/postgresql.js
+++ src/database/postgresql.js
@@ -3,9 +3,9 @@ const { Pool } = pkg;
 
 export const database = new Pool({
   user: "admin",
-  host:  "dpg-cjb4r5pitvpc73ekc0b0-a.singapore-postgres.render.com",
-  database:  "hoainhan_m6h5",
-  password:  "NSP0G2rDxpjNWkkNLdvqfmKxBFJeSKcV",
+  host:  "dpg-ckqigau2eoec73adrdj0-a.singapore-postgres.render.com",
+  database:  "mtt2023_t5s2",
+  password:  "98d3mUgTbA5fN0IB54LpGxMxoyVmdA4E",
   port: 5432, 
   ssl: {
     rejectUnauthorized: false 
@@ -15,7 +15,7 @@ export const database = new Pool({
 export async function connectDB() {
   database.connect()
   .then(() => console.log('Connected to the database'))
-  .catch(err => console.error('Error connecting to the database', err));
+  .catch(err => console.error('Error connecting to the database: ', err));
 
 }
 
