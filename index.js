3c9c6cb6980b6027264dca05381a724795a42575
Nhan Le <nhan@treehouse.finance>
2023-10-22 22:03:29 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { initRouter } from "./src/route/router.js";
-import swaggerDocs from "./src/utils/swagger.js";
 import { connectDB } from "./src/database/postgresql.js";
 
 const app = express();
@@ -13,5 +12,4 @@ await connectDB();
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`API server started on port ${port}`);
-    swaggerDocs(app, port);
 });
