fe1abb67e156a501252f8f7531845e0804f0348a
Nhan Le <nhan@treehouse.finance>
2023-11-12 16:03:43 +0700

    add msg statistics

--- src/handler/statistic.js
+++ src/handler/statistic.js
@@ -1,5 +1,6 @@
 import {Status, StatusCode} from "../common/common.js";
 import StatisticBusiness from "../business/statistic/statistic.js";
+import { MSG } from "../common/message.js";
 
 
 async function getGeneral(req, res) {
@@ -8,19 +9,19 @@ async function getGeneral(req, res) {
         if (items == null) {
             res.status(StatusCode.BAD_REQUEST).json({
                 status: Status.FAILED,
-                message: "Failed to get general statistic",
+                message: MSG.GET_STATISTIC_GENERAL_FAILED,
             });
             return
         }
         res.status(StatusCode.OK).json({
             status: Status.OK,
-            message: "Get general statistic successfully!",
+            message: MSG.GET_STATISTIC_GENERAL_SUCCESS,
             data: items
         }); 
     } catch (error) {
+        console.log("INTERNAL_SERVER_ERROR: ", error.message)
         res.status(StatusCode.INTERNAL_SERVER).json({
-            status: Status.ERROR,
-            message: error.message,
+            status: Status.INTERNAL_SERVER_ERROR,
         });
     }
 
@@ -32,88 +33,89 @@ async function getTopProduct(req, res) {
         if (items == null) {
             res.status(StatusCode.BAD_REQUEST).json({
                 status: Status.FAILED,
-                message: "Failed to get top product",
+                message: MSG.GET_TOP_PRODUCT_FAILED,
             });
             return
         }
         res.status(StatusCode.OK).json({
             status: Status.OK,
-            message: "Get top product successfully!",
+            message: MSG.GET_TOP_PRODUCT_SUCCESS,
             data: items
         }); 
     } catch (error) {
+        console.log("INTERNAL_SERVER_ERROR: ", error.message)
         res.status(StatusCode.INTERNAL_SERVER).json({
-            status: Status.ERROR,
-            message: error.message,
-        });
-    }
-
-}
-async function getOutOfStock(req, res) {
-    try {
-        const {page, limit} = req.query
-        const items = await getOutOfStock(page, limit);
-        if (items == null) {
-            res.status(StatusCode.BAD_REQUEST).json({
-                status: Status.FAILED,
-                message: "Failed to out of stock product",
-            });
-            return
-        }
-        res.status(StatusCode.OK).json({
-            status: Status.OK,
-            message: "Get out of stock product successfully!",
-            data: items
-        }); 
-    } catch (error) {
-        res.status(StatusCode.INTERNAL_SERVER).json({
-            status: Status.ERROR,
-            message: error.message,
+            status: Status.INTERNAL_SERVER_ERROR,
         });
     }
 
 }
 
-async function getOwnOrder(req, res) {
-    try {
-        const token = getJwt(req)
-        if(token == null) {
-            res.status(StatusCode.BAD_REQUEST).json({
-                status: Status.FAILED,
-                message: "Not verified!",
-            });
-            return
-        }
-        const payload = decodeJwt(token)
-        if(payload == null) {
-            res.status(StatusCode.BAD_REQUEST).json({
-                status: Status.FAILED,
-                message: "Not verified!",
-            });
-            return
-        }
-        const {page, limit} = req.query
-        const items = await getOwnOrder(payload.id, page, limit);
-        if (items == null) {
-            res.status(StatusCode.BAD_REQUEST).json({
-                status: Status.FAILED,
-                message: "Failed to get order",
-            });
-            return
-        }
-        res.status(StatusCode.OK).json({
-            status: Status.OK,
-            message: "Get order successfully!",
-            data: items
-        }); 
-    } catch (error) {
-        res.status(StatusCode.INTERNAL_SERVER).json({
-            status: Status.ERROR,
-            message: error.message,
-        });
-    }
+// async function getOutOfStock(req, res) {
+//     try {
+//         const {page, limit} = req.query
+//         const items = await getOutOfStock(page, limit);
+//         if (items == null) {
+//             res.status(StatusCode.BAD_REQUEST).json({
+//                 status: Status.FAILED,
+//                 message: "Failed to out of stock product",
+//             });
+//             return
+//         }
+//         res.status(StatusCode.OK).json({
+//             status: Status.OK,
+//             message: "Get out of stock product successfully!",
+//             data: items
+//         }); 
+//     } catch (error) {
+//         res.status(StatusCode.INTERNAL_SERVER).json({
+//             status: Status.ERROR,
+//             message: error.message,
+//         });
+//     }
 
-}
+// }
+
+// async function getOwnOrder(req, res) {
+//     try {
+//         const token = getJwt(req)
+//         if(token == null) {
+//             res.status(StatusCode.BAD_REQUEST).json({
+//                 status: Status.FAILED,
+//                 message: "Not verified!",
+//             });
+//             return
+//         }
+//         const payload = decodeJwt(token)
+//         if(payload == null) {
+//             res.status(StatusCode.BAD_REQUEST).json({
+//                 status: Status.FAILED,
+//                 message: "Not verified!",
+//             });
+//             return
+//         }
+//         const {page, limit} = req.query
+//         const items = await getOwnOrder(payload.id, page, limit);
+//         if (items == null) {
+//             res.status(StatusCode.BAD_REQUEST).json({
+//                 status: Status.FAILED,
+//                 message: "Failed to get order",
+//             });
+//             return
+//         }
+//         res.status(StatusCode.OK).json({
+//             status: Status.OK,
+//             message: "Get order successfully!",
+//             data: items
+//         }); 
+//     } catch (error) {
+//         res.status(StatusCode.INTERNAL_SERVER).json({
+//             status: Status.ERROR,
+//             message: error.message,
+//         });
+//     }
+
+// }
 
 const StatisticHandler = {
     getGeneral,
